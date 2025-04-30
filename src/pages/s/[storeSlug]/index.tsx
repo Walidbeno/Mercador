0c6ebfd087dd10bf552fdb451a0fe9e0bc1ca52a
Walid Beno <walidbenoihi@gmail.com>
2025-04-30 10:35:11 +0100

    XXX5

--- src/pages/s/[storeSlug]/index.tsx
+++ src/pages/s/[storeSlug]/index.tsx
@@ -4,6 +4,7 @@ import prisma from '@/lib/prisma';
 interface StoreProduct {
   id: string;
   product: {
+    id: string;
     title: string;
     description: string;
     shortDescription: string | null;
